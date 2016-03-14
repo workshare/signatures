3cc0b7f09e1917bac1c9a55c8850a3f920d8611a
Jesus Prieto Colomina <chus1818@gmail.com>
2016-03-14 14:08:20 +0000

    Expiration concept added

--- lib/signatures/validators/basic.rb
+++ lib/signatures/validators/basic.rb
@@ -1,20 +1,27 @@
 module Signatures
   module Validators
     class Basic
-      attr_accessor :keystore, :signer
+      attr_accessor :keystore, :signer, :expiration_time, :clock
 
-      def initialize(keystore: {}, signer: Signers::Keystore.new(keystore: keystore))
+      def initialize(keystore: {}, signer: Signers::Keystore.new(keystore: keystore), expiration_time: 900, clock: Time)
         self.signer = signer
+        self.expiration_time = expiration_time
+        self.clock = clock
       end
 
       def call(to_validate:, signature:, **args)
-        gen_sig = signer.call(to_validate, args)
-        gen_sig == signature
+        !expired_signature?(to_validate) &&
+        signer.call(to_validate, args) == signature
       end
 
       private
 
-      attr_writer :keystore, :signer
+      def expired_signature?(signature_params)
+        signature_params[:timestamp] &&
+        (clock.now.to_i - signature_params[:timestamp]) > expiration_time
+      end
+
+      attr_writer :keystore, :signer, :expiration_time, :clock
     end
   end
 end
--- spec/lib/validators/basic_spec.rb
+++ spec/lib/validators/basic_spec.rb
@@ -1,28 +1,59 @@
 require 'spec_helper'
 
 describe Signatures::Validators::Basic do
-  subject { described_class.new signer: signer }
+  subject { described_class.new signer: signer, expiration_time: expiration_time, clock: clock }
   let(:signer) { double call: true }
+  let(:expiration_time) { 10 }
+  let(:clock) { double 'clock', now: time_now }
+  let(:time_now) { double :time_now, to_i: (timestamp + time_now_from_timestamp) }
+  let(:time_now_from_timestamp) { 5 }
+  let(:timestamp) { 100 }
 
   describe '#call' do
     before do
       allow(signer).to receive(:call)
-        .with(:to_validate, key: :key).and_return(signed)
+        .with(to_validate, key: :key).and_return(signed)
     end
 
-    context 'when the signature for the given text is the same as the signature received' do
-      let(:signed) { :signature }
+    context 'when there is no timestamp in the signature' do
+      let(:to_validate) { { to_validate: :to_validate } }
 
-      it 'returns true' do
-        expect(subject.call(to_validate: :to_validate, signature: :signature, key: :key)).to be true
+      context 'when the signature for the given text is the same as the signature received' do
+        let(:signed) { :signature }
+
+        it 'returns true' do
+          expect(subject.call(to_validate: to_validate, signature: :signature, key: :key)).to be true
+        end
+      end
+
+      context 'when the signature for the given text is not the same as the signature received' do
+        let(:signed) { :something }
+
+        it 'returns false' do
+          expect(subject.call(to_validate: to_validate, signature: :signature, key: :key)).to be false
+        end
       end
     end
 
-    context 'when the signature for the given text is not the same as the signature received' do
-      let(:signed) { :something }
+    context 'when there is timestamp in the signature' do
+      let(:to_validate) { { to_validate: :to_validate, timestamp: timestamp } }
+      let(:time_now_from_timestamp) { 5 }
+      let(:signed) { :signature }
 
-      it 'returns false' do
-        expect(subject.call(to_validate: :to_validate, signature: :signature, key: :key)).to be false
+      context 'when it is expired' do
+        let(:expiration_time) { 2 }
+
+        it 'returns false' do
+          expect(subject.call(to_validate: to_validate, signature: :signature, key: :key)).to be false
+        end
+      end
+
+      context 'when it is not expired' do
+        let(:expiration_time) { 10 }
+
+        it 'returns true' do
+          expect(subject.call(to_validate: to_validate, signature: :signature, key: :key)).to be true
+        end
       end
     end
   end
